c15d25a8b1d614cb6b0f163c28491bfe8f57ccca
Nikola Djekic <nikoladj.21@gmail.com>
2020-06-16 22:02:20 +0200

    add basic home component logic + service

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MockUpService } from 'src/app/services/mock-up.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allPostsArr: Array<any> = [];
 
-  constructor() { }
+  constructor(private _service: MockUpService) {}
 
   ngOnInit(): void {
+    this.allPostsArr = [];
+    this._service.getPosts().subscribe((posts) => {
+      this.allPostsArr = posts;
+    });
   }
 
+  // delete a post from the array
+  deletePost(post): void {
+    const postToRemove = this.allPostsArr.indexOf(post);
+    if (postToRemove > -1) {
+      this.allPostsArr.splice(postToRemove, 1);
+    }
+  }
 }
--- client/src/app/services/mock-up.service.ts
+++ client/src/app/services/mock-up.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MockUpService {
+  url: string = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+
+  getPosts() {
+    return this._http.get<any>(this.url);
+  }
 }
