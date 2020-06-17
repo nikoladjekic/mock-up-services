8c0095004e8955faf8cb28c6ddc906144ffd25d2
Nikola Djekic <nikoladj.21@gmail.com>
2020-06-17 11:02:57 +0200

    update services

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -11,9 +11,11 @@ import { Post } from 'src/app/models/post.model';
 })
 export class HomeComponent implements OnInit {
   successMessage: boolean = false;
+  successEdit: boolean = false;
   errorMessage: boolean = false;
 
   allPostsArr: Array<Post> = [];
+  editThisPost: Post;
   searchTerm: string;
 
   constructor(
@@ -31,11 +33,11 @@ export class HomeComponent implements OnInit {
     this.allPostsArr = [];
     this._service.getPosts().subscribe((posts) => {
       this.allPostsArr = posts;
+      this._spinner.hide();
     });
-    this._spinner.hide();
   }
 
-  // delete a post from the array
+  // remove specific post from the array
   deletePost(post): void {
     const postToRemove = this.allPostsArr.indexOf(post);
     if (postToRemove > -1) {
@@ -43,13 +45,40 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  // add new post
+  // add new post on top of the list
   onSubmit(post): void {
-    this.successMessage = true;
-    let newPost: Post = new Post(post.userId, post.id, post.title, post.body);
-    this.allPostsArr.unshift(newPost);
-    setTimeout(() => {
-      this.successMessage = false;
-    }, 2000);
+    this.successMessage = false;
+    this._spinner.show();
+    let newPost = {
+      userId: post.userId,
+      title: post.title,
+      body: post.body,
+    };
+    this._service.addNewPost(newPost).subscribe((resp) => {
+      this.allPostsArr.unshift(resp);
+      this._spinner.hide();
+      this.successMessage = true;
+    });
+  }
+
+  // when user clicks on edit populate all fields with exiting data
+  editPost(updatedPost): void {
+    this.successEdit = false;
+    this.editThisPost = updatedPost;
+  }
+
+  // when user confirms the changes
+  confirmEdit(): void {
+    this._spinner.show();
+    this._service.updatePost(this.editThisPost).subscribe((resp) => {
+      // with ng model we have already updated our data, but
+      // for demonstration purposes we will remove old post, and then
+      this.deletePost(this.editThisPost);
+      // we will add to top of the list new post that we receive as a response
+      this.allPostsArr.unshift(resp);
+
+      this._spinner.hide();
+      this.successEdit = true;
+    });
   }
 }
--- client/src/app/services/mock-up.service.ts
+++ client/src/app/services/mock-up.service.ts
@@ -11,7 +11,26 @@ export class MockUpService {
 
   constructor(private _http: HttpClient) {}
 
+  // methods that are used
   getPosts() {
     return this._http.get<Array<Post>>(this.url);
+  }
+
+  addNewPost(newPost) {
+    return this._http.post<any>(this.url, newPost, {
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    });
+  }
+
+  updatePost(updatedPost) {
+    return this._http.put<any>(this.url + '/1', updatedPost, {
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    });
+  }
+
+  // other methods that could be used, but are not working correctly,
+  // so i replicated them inside home component over an array that we get
+  deletePost() {
+    return this._http.delete<any>(this.url + '/1');
   }
 }
